b557aa81aa41f7156d37c871cb6f16264d327dc2
John Bell <jbellny@gmail.com>
2022-04-05 15:35:52 -0400

    connect api to front-end

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,34 @@
+<script context="module">
+    import requests from '../data/requests'
+    export async function load() {
+        try {
+            const usStats = await requests.usStats()
+
+            return { props: { usStats }}
+
+        } catch(e) {
+
+        }
+    }
+</script>
+
 <script>
     import CovidStat from "../components/CovidStat.svelte";
     import CovidChart from "../components/CovidChart.svelte";
     import TableContainer from "../components/TableContainer.svelte";
+    export let usStats
+    console.log('US-Stats', usStats)
 </script>
 
+<svelte:head>
+    <title>Void 19 US Tracker</title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+        <h1>Covid 19 - US</h1>
+    </div>
+</div>
 <CovidStat/>
 <CovidChart/>
 <TableContainer/>
